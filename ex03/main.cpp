ac74639ddc5f7fbaf2da37251e49ff8ff1998bf3
Laura Garcia Arch <garch.laura@gmail.com>
2024-02-18 16:34:34 +0100

    cpp05

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: laugarci <laugarci@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 15:02:19 by laugarci          #+#    #+#             */
-/*   Updated: 2024/02/16 15:01:14 by laugarci         ###   ########.fr       */
+/*   Updated: 2024/02/18 16:34:25 by laugarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,21 +30,30 @@ int main()
 	Intern intern1;
 	Intern intern2;
 	Intern intern3;
-
-	AForm *robot = intern1.makeForm("robotomy request", "Manuel");
-	AForm *presi = intern2.makeForm("presidential pardon", "Pepito");
-	AForm *shr = intern3.makeForm("shrubbery creation", "Menganito");
-
-	Bureaucrat bur("test1", 45);
-	robot->execute(bur);
-	Bureaucrat bur1("test2", 5);
-	presi->execute(bur1);
-	Bureaucrat bur2("test3", 137);
-	shr->execute(bur2);
 	
 	try {
-	AForm *test = intern1.makeForm("test", "test");
-	test->execute(bur2);
+		AForm *robot = intern1.makeForm("robotomy request", "Manuel");
+		AForm *presi = intern2.makeForm("presidential pardon", "Pepito");
+		AForm *shr = intern3.makeForm("shrubbery creation", "Menganito");
+
+		Bureaucrat bur("test1", 45);
+		robot->execute(bur);
+		Bureaucrat bur1("test2", 5);
+		presi->execute(bur1);
+		Bureaucrat bur2("test3", 137);
+		shr->execute(bur2);
+		delete robot;
+		delete presi;
+		delete shr;
+	} catch (std::exception &e) {
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+	
+	std::cout << BGRED "[ INCORRECT TEST ]" RESET << std::endl;
+	try {
+		AForm *test = intern1.makeForm("test", "test");
+		Bureaucrat bur3("test", 5);
+		test->execute(bur3);
 	}
 	catch (std::exception &e)
 	{
